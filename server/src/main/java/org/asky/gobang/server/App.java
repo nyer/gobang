96fe81a9b03b51d1e7cffb14a66c789081ce0999
notifyer <notifyer@server>
2013-02-24 13:37:42 +0800

    init project struct

---
+++ server/src/main/java/org/asky/gobang/server/App.java
@@ -0,0 +1,13 @@
+package org.asky.gobang.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
